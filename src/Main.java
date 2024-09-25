d64bf1c5551b3817d9f8f96b0cc009de6cb929f2
Jubaars <43658933+RafaelVi@users.noreply.github.com>
2024-09-24 21:16:12 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -52,32 +52,38 @@ public class Main {
 
         int[] pontuacao = new int[qtdEquipes];
 
-        for (int i = 0; i < qtdEquipes; i++) {//duelo todos contra todos
-            for (int j = 0; j < qtdEquipes - 1; j++) {
-                if (i > j){//garante que uma equipe nao se enfrente nem enfrente outras mais de 1 vez(recomendo desenhar
-                            //para entender a lógica.
-                    int ptI = random.nextInt(10);
-                    int ptJ = random.nextInt(10);
-
-                    System.out.println("Equipe 1: "+ equipes[i] + " " + ptI);
-                    System.out.println("Equipe 2: "+ equipes[j] + " " + ptJ);
-                    if (ptI > ptJ){// Primeiro time venceu
-                        pontuacao[i] += 1;
-                    } else if (ptJ > ptI) {// Segundo time venceu
-                        pontuacao[j] += 1;
-                    } else {//Empate
-                        if (notasDesign[i] > notasDesign[j]) pontuacao[i] += 1;//Verifica nota de design pra desempatar
-                        else if(notasDesign[i] < notasDesign[j]) pontuacao[j] += 1;
-
-                    }
-                }
-            }
-        }
+        executaDuelos(false, equipes, pontuacao, notasDesign);
         //fim da parte 2
 
         //inicio da parte 3
-        for (int i = 0; i < qtdEquipes; i++) {// percorre todas as equipes
-            for (int j = 0; j < qtdEquipes; j++) {// compara a pontuacao equipe com todos as outras
+        ordenaEquipes(pontuacao, notasDesign, equipes);
+
+        for (int i = 0; i < equipes.length; i++) {
+            System.out.println((i + 1) + "° colocado(a): Equipe " + equipes[i] + " com a pontuação de " + pontuacao[i] +
+                    (pontuacao[i] > 1 ? " pontos." : " ponto."));
+        }
+
+        int[] finalistas = {equipes[0], equipes[1], equipes[2]};
+        System.out.println("As 3 equipes finalistas são:");
+        for (int finalista : finalistas){
+            System.out.println("Equipe " + finalista);
+        }
+
+        executaDuelos(true,finalistas, pontuacao, notasDesign);
+
+        ordenaEquipes(pontuacao, notasDesign, finalistas);
+
+        for (int i = 0; i < finalistas.length; i++) {
+            System.out.println((i + 1) + "° colocado(a): Equipe " + finalistas[i] + " com a pontuação de " + pontuacao[i] +
+                    (pontuacao[i] > 1 ? " pontos." : " ponto."));
+        }
+
+
+    }
+
+    private static void ordenaEquipes(int[] pontuacao, double[] notasDesign, int[] equipes) {
+        for (int i = 0; i < equipes.length; i++) {// percorre todas as equipes
+            for (int j = 0; j < equipes.length; j++) {// compara a pontuacao equipe com todos as outras
                 if (i != j) {
                     boolean ehMaior = false;
                     if (pontuacao[i] > pontuacao[j]) {
@@ -99,12 +105,43 @@ public class Main {
             }
             //fim da parte 3
         }
+    }
 
-        for (int i = 0; i < equipes.length; i++) {
-            System.out.println((i + 1) + "° colocado(a): Equipe " + equipes[i] + " com a pontuação de " + pontuacao[i] +
-                    (pontuacao[i] > 1 ? " pontos." : " ponto."));
+    private static void executaDuelos(boolean ehFinal, int[] equipes, int[] pontuacao, double[] notasDesign) {
+        Random random = new Random();
+        if (ehFinal) {
+            for (int i = 0; i < equipes.length; i++) {
+                pontuacao[i] = 0;
+            }
         }
+        for (int i = 0; i < equipes.length; i++) {//duelo todos contra todos
+            for (int j = 0; j < equipes.length - 1; j++) {
+                if (i > j) {//garante que uma equipe nao se enfrente nem enfrente outras mais de 1 vez(recomendo desenhar
+                    //para entender a lógica.
+                    int ptI = random.nextInt(10);
+                    int ptJ = random.nextInt(10);
 
+                    System.out.println("Equipe 1: " + equipes[i] + " " + ptI);
+                    System.out.println("Equipe 2: " + equipes[j] + " " + ptJ);
+                    if (ptI > ptJ) {// Primeiro time venceu
+                        if (ehFinal) pontuacao[i] += 2;
+                        else pontuacao[i] += 1;
+                    } else if (ptJ > ptI) {// Segundo time venceu
+                        if (ehFinal) pontuacao[j] += 2;
+                        else pontuacao[j] += 1;
+                    } else {//Empate
+                        if (!ehFinal) {
+                            if (notasDesign[i] > notasDesign[j])
+                                pontuacao[i] += 1;//Verifica nota de design pra desempatar
+                            else if (notasDesign[i] < notasDesign[j]) pontuacao[j] += 1;
+                        } else {
+                            pontuacao[i] += 1;
+                            pontuacao[j] += 1;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private static void trocaPosicao(int[] vetor, int i, int j) {
